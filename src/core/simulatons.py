ac9ea36d13addf85ca24f8e9fe5b33b3e3c38b57
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-11-16 17:44:45 +0300

    добавлен интерфейс PID

--- src/core/simulatons.py
+++ src/core/simulatons.py
@@ -3,11 +3,12 @@ import logging
 from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
 
 # Коэффициенты в уравнении потерь при нагревании печи, полученные из docs/oven_model
-A1 = -0.004511200300912059
-A2 = 2.97936894852502
-A3 = -1070.2463559702983
-B1 = 62.45356998903521
-B2 = 2.2915522070523324
+A1 = 0.004433988115689157
+A2 = -2.1383429579319655
+A3 = 537.4230466267185
+B1 = 730.5861414838458
+B2 = 595.3779940428719
+K_COEFF = 0.49505773085774213
 # Коэффициенты в уравнении охлаждения печи
 C1 = -3.89357385551864e-06
 C2 = -0.21203098043962063
@@ -37,12 +38,22 @@ def quartz_heat_capacity(temperature):
     return 931.3 + 0.256 * temperature - 24 * temperature ** (-2)
 
 
-def get_dt(heat_flow, power, t, a1, a2, a3, b1, b2):
+def get_dt(heat_flow, power, t, a1, a2, a3, b1, b2, k_coeff):
     t_increase = heat_flow / quartz_heat_capacity(t)
+
     t_t_loss = (a1 * t**2 + a2 * t + a3) / quartz_heat_capacity(t)
+    if t_t_loss < 0:
+        t_t_loss = 0
+
     power_t_loss = (b1 * power + b2) / quartz_heat_capacity(t)
+    if power_t_loss < 0:
+        power_t_loss = 0
+
+    if power_t_loss > k_coeff * t_t_loss:
+        power_t_loss = k_coeff * t_t_loss
+
     dt = t_increase - cooling_t_loss(t) - t_t_loss - power_t_loss
-    return t + (dt / AGG_TIME)
+    return t + dt
 
 
 class PIDSimulations(QObject):
@@ -61,7 +72,7 @@ class PIDSimulations(QObject):
 
         for _ in range(sim_time):
             # новая температура через 1 секнду
-            new_temperature = get_dt(heat_flow, power, current_temperature, A1, A2, A3, B1, B2)
+            new_temperature = get_dt(heat_flow, power, current_temperature, A1, A2, A3, B1, B2, K_COEFF)
             oven_temperatures.append(new_temperature)
             current_temperature = new_temperature
         return oven_temperatures
@@ -81,19 +92,48 @@ class PIDSimulations(QObject):
         self.logger.info(f"Received data for simulation: {data}")
 
         initial_temp = data.get("initial_temp", 0)
-        final_temperature = data.get("final_temp", 0)
-        heating_rate = data.get("heating_rate", final_temperature - initial_temp)
+        final_temp = data.get("final_temp", 0)
+        heating_rate = data.get("heating_rate", final_temp - initial_temp)
         sim_time = data.get("sim_time", 0)
+        kp = data.get("kp", 0)
+        ki = data.get("ki", 0)
+        kd = data.get("kd", 0)
 
-        # задаемся мощностью для отладки
-        power = 20
+        dt = 1
+        num_steps = int(sim_time / dt)
+        time_array = [i * dt for i in range(num_steps + 1)]
+        # TODO учесть временной интервал
+        target_temperatures = self._calculate_target_curve(initial_temp, final_temp, heating_rate, num_steps)
 
-        time_array = list(range(sim_time + 1))  # в секндах
-        target_temperatures = self._calculate_target_curve(initial_temp, final_temperature, heating_rate, sim_time)
-        oven_temperatures = self._calculate_oven_curve(initial_temp, power, sim_time)
+        current_temperature = initial_temp
+        oven_temperatures = [current_temperature]
 
-        # Посылаем сигнал для температуры печи
-        self.simulations_data_signal.emit({"x": time_array, "y": oven_temperatures, "label": "oven_temperatures"})
+        initial_target_temp = target_temperatures[0]
+        initial_error = initial_target_temp - current_temperature
+        errors = [initial_error]
 
-        # Посылаем сигнал для целевой температуры
-        self.simulations_data_signal.emit({"x": time_array, "y": target_temperatures, "label": "target_temperatures"})
+        integral_error = initial_error * dt
+        previous_error = initial_error
+
+        for time_step in range(num_steps):
+            target_temperature = target_temperatures[time_step]
+            error = target_temperature - current_temperature
+            errors.append(error)
+
+            integral_error += error * dt
+            derivative_error = (error - previous_error) / dt
+            power = kp * error + ki * integral_error + kd * derivative_error
+            power = max(0, min(power, 100))
+
+            amperage = (MAINS_VOLTAGE / OVEN_RESISTANCE) * power / 100
+            heat_flow = amperage * MAINS_VOLTAGE * dt
+            new_temperature = get_dt(heat_flow, power, current_temperature, A1, A2, A3, B1, B2, K_COEFF)
+            oven_temperatures.append(new_temperature)
+            current_temperature = new_temperature
+            previous_error = error
+
+        self.logger.info(f"Oven temperature: {oven_temperatures}")
+
+        self.simulations_data_signal.emit({"x": time_array, "y": oven_temperatures, "label": "oven_temperature"})
+        self.simulations_data_signal.emit({"x": time_array, "y": target_temperatures, "label": "target_temperature"})
+        self.simulations_data_signal.emit({"x": time_array, "y": errors, "label": "error"})  # Emit error data
